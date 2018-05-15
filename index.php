bcc30594adbee81a6bab6605c3e7e994feba250f
Shane Lennon <shanelennon7blue@gmail.com>
2018-05-15 12:52:52 +0100

    initial hello world

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>Hello</title>
+	</head>
+
+	<body>
+		<h2>Hello Wolrd</h2>
+		<ul>
+			<?php 
+			
+				for ($i = 0; $i < 10; $i ++) {
+					echo "<li> Index Number: " . $i . "</li>";
+				}
+			?>
+		</ul>
+	</body>
+</html>
